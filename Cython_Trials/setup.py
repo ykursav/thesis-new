eeef8a6a0a6971ccddf67a4e515c38af84204294
Emre Yunus <Emre Yunus>
2016-10-22 23:58:32 +0200

    Cython new extraction.c created with ctypes

---
+++ Cython_Trials/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+import numpy
+import pyximport
+pyximport.install(setup_args={"script_args":["--compiler=mingw32"],
+                              "include_dirs":numpy.get_include()},
+                  reload_support=True)
+
+setup(
+    ext_modules=cythonize("extraction_cython.pyx"),
+    include_dirs=[numpy.get_include()]
+)
