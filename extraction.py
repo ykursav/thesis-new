78905406fde6e36cb3ab6fb74f620c61755f9247
Emre Yunus <Emre Yunus>
2016-11-18 10:21:39 +0100

    extraction class converted functions from class manner

--- extraction.py
+++ extraction.py
@@ -15,62 +15,46 @@ libextraction.sum.argtypes = [_doublepp, ctypes.c_int]
 libextraction.calculateSD.restype = ctypes.c_double
 setUseOptimized(True)
 ##gc.enable()
+N = 0
+M = 0
+L = 0
+image = array([])
+width = 0
+height = 0
+def set_initials(N, M, L, image):
+    N = N
+    M = M
+    L = L
+    image = image
 
-def unwrap_self_f(arg, **kwarg):
-    return SignatureExtraction.get_average_luminance_of_block(*arg, **kwarg)
+number_of_blocks = ((L - N) / M ) + 1
 
-class SignatureExtraction:
-    '''N block size, M overlapping pixels, L image size'''
-    def __init__(self, N, M, L):
-        self.L = L
-        self.N = N
-        self.M = M
-        self.image = array([])
-        #self.image = image
-        self.width = 0
-        self.height = 0
-        self.number_of_blocks = ((self.L - self.N) / self.M ) + 1
-        self.block_all = zeros((self.number_of_blocks, self.number_of_blocks, self.N, self.N))
-
-
-##    @profile
-##    def get_width_height(self, image):
-##        self.height = len(image)
-##        self.width = len(image[0,:])
-
-
-    #@profile
-    def set_image(self, image):
-        self.image = image
-
-    #@profile
-    def get_average_luminance_of_block(self, block):
-        '''luminance calculation block'''
+def get_average_luminance_of_block(block):
+    '''luminance calculation block'''
 ##        blockpp = (block.__array_interface__['data'][0] + np.arange(block.shape[0]) * block.strides[0]).astype(np.uintp)
 ##        block_sum = libextraction.sum(blockpp, ctypes.c_int(self.N))
-        #print block_sum
-        return sum(block) / (self.N * self.N)
+    #print block_sum
+    return sum(block) / (N * N)
 
-    #@profile
-    def get_blocks(self):
-        '''Dividing cropped image N x N blocks by M overlapping'''
-        I_vis_blur_y = zeros((self.number_of_blocks * self.N, self.number_of_blocks * self.N))
-        I_vis_blur_x = zeros((self.L, self.number_of_blocks * self.N))
-        for x in xrange(0, self.L - self.M, self.M):
-            I_vis_blur_x[:, x * 2:x * 2 + self.N] = self.image[:, x:x + self.N]
+def get_blocks():
+    '''Dividing cropped image N x N blocks by M overlapping'''
+    I_vis_blur_y = zeros((number_of_blocks * N, number_of_blocks * N))
+    I_vis_blur_x = zeros((L, number_of_blocks * N))
+    for x in xrange(0, L - M, M):
+        I_vis_blur_x[:, x * 2:x * 2 + N] = image[:, x:x + N]
 
-        for y in xrange(0, self.L - self.M, self.M):
-            I_vis_blur_y[y * 2:y * 2 + self.N, :] = I_vis_blur_x[y:y + self.N, :]
+    for y in xrange(0, L - M, M):
+        I_vis_blur_y[y * 2:y * 2 + N, :] = I_vis_blur_x[y:y + N, :]
 
 
-        return I_vis_blur_y
-    #@profile
-    def basic_rotations(self, image):
-        center = (self.N * self.number_of_blocks) / 2
-        rot_matrix = getRotationMatrix2D((center, center), 90, 1)
-        rot90 = warpAffine(image, rot_matrix, (center * 2, center * 2))
-        rot180 = warpAffine(rot90, rot_matrix, (center * 2, center * 2))
-        rot270 = warpAffine(rot180, rot_matrix, (center * 2, center * 2))
+    return I_vis_blur_y
+
+def basic_rotations():
+    center = (N * number_of_blocks) / 2
+    rot_matrix = getRotationMatrix2D((center, center), 90, 1)
+    rot90 = warpAffine(image, rot_matrix, (center * 2, center * 2))
+    rot180 = warpAffine(rot90, rot_matrix, (center * 2, center * 2))
+    rot270 = warpAffine(rot180, rot_matrix, (center * 2, center * 2))
 ##        fVertical0 = flip(image, 0)
 ##        fHorizontal0 = flip(image, 1)
 ##        fVertical90 = flip(rot90, 0)
@@ -80,40 +64,42 @@ class SignatureExtraction:
 ##        fVertical270 = flip(rot270, 0)
 ##        fHorizontal270 = flip(rot270, 1)
 
-        return rot90, rot180, rot270
+    return rot90, rot180, rot270
 ##    fVertical0, fHorizontal0, fVertical90, fHorizontal90, fVertical180, fHorizontal180, \
 ##         fVertical270, fHorizontal270
 
-    #@profile
-    def get_fragment(self, rot0, rot90, rot180, rot270, x, y, only_rotate):
 
-        p = Pool(processes=4)
-        blocks = [rot0[y * 8:y * 8 + self.N, x * 8:x * 8 + self.N], rot90[y * 8:y * 8 + self.N], rot180[y * 8: y * 8 + self.N, x * 8:x * 8 + self.N], rot270[y * 8:y * 8 + self.N,x * 8:x * 8 + self.N]]
-        if only_rotate == 1:
-            [lum1, lum2, lum3, lum4] = p.map(unwrap_self_f, blocks)
-            #lum1 = self.get_average_luminance_of_block(rot0[y * 8:y * 8 + self.N, x * 8:x * 8 + self.N])
-            #lum2 = self.get_average_luminance_of_block(rot90[y * 8:y * 8 + self.N, x * 8:x * 8 + self.N])
-            #lum3 = self.get_average_luminance_of_block(rot180[y * 8:y * 8 + self.N, x * 8:x * 8 + self.N])
-            #lum4 = self.get_average_luminance_of_block(rot270[y * 8:y * 8 + self.N, x * 8:x * 8 + self.N])
-            avg_lum = (lum1 + lum2 + lum3 + lum4) / 4
-            #std_lum = np.std(np.array([lum1, lum2, lum3, lum4]))
-            
-            std_lum = libextraction.calculateSD(array([lum1, lum2, lum3, lum4]).ctypes.data_as(c_void_p))
-            
-            return avg_lum, std_lum
 
-        elif only_rotate == -1:
-            lum1 = self.get_average_luminance_of_block(rot0[y * 8:y * 8 + self.N, x * 8:x * 8 + self.N])
-            avg_lum = lum1
-            std_lum = 0
-            
-            return avg_lum, std_lum
 
-        else:
-            lum1 = self.get_average_luminance_of_block(rot0[y * 8:y * 8 + self.N, x * 8:x * 8 + self.N])
-            lum2 = self.get_average_luminance_of_block(rot90[y * 8:y * 8 + self.N, x * 8:x * 8 + self.N])
-            lum3 = self.get_average_luminance_of_block(rot180[y * 8:y * 8 + self.N, x * 8:x * 8 + self.N])
-            lum4 = self.get_average_luminance_of_block(rot270[y * 8:y * 8 + self.N, x * 8:x * 8 + self.N])
+def get_fragment(rot0, rot90, rot180, rot270, x, y, only_rotate):
+
+    p = Pool(processes=4)
+    blocks = [rot0[y * 8:y * 8 + N, x * 8:x * 8 + N], rot90[y * 8:y * 8 + N], rot180[y * 8: y * 8 + N, x * 8:x * 8 + N], rot270[y * 8:y * 8 + N,x * 8:x * 8 + N]]
+    if only_rotate == 1:
+        [lum1, lum2, lum3, lum4] = p.map(unwrap_self_f, blocks)
+        #lum1 = self.get_average_luminance_of_block(rot0[y * 8:y * 8 + self.N, x * 8:x * 8 + self.N])
+        #lum2 = self.get_average_luminance_of_block(rot90[y * 8:y * 8 + self.N, x * 8:x * 8 + self.N])
+        #lum3 = self.get_average_luminance_of_block(rot180[y * 8:y * 8 + self.N, x * 8:x * 8 + self.N])
+        #lum4 = self.get_average_luminance_of_block(rot270[y * 8:y * 8 + self.N, x * 8:x * 8 + self.N])
+        avg_lum = (lum1 + lum2 + lum3 + lum4) / 4
+        #std_lum = np.std(np.array([lum1, lum2, lum3, lum4]))
+        
+        std_lum = libextraction.calculateSD(array([lum1, lum2, lum3, lum4]).ctypes.data_as(c_void_p))
+        
+        return avg_lum, std_lum
+
+    elif only_rotate == -1:
+        lum1 = get_average_luminance_of_block(rot0[y * 8:y * 8 + N, x * 8:x * 8 + N])
+        avg_lum = lum1
+        std_lum = 0
+        
+        return avg_lum, std_lum
+
+    else:
+        lum1 = get_average_luminance_of_block(rot0[y * 8:y * 8 + N, x * 8:x * 8 + N])
+        lum2 = get_average_luminance_of_block(rot90[y * 8:y * 8 + N, x * 8:x * 8 + N])
+        lum3 = get_average_luminance_of_block(rot180[y * 8:y * 8 + N, x * 8:x * 8 + N])
+        lum4 = get_average_luminance_of_block(rot270[y * 8:y * 8 + N, x * 8:x * 8 + N])
 ##            lum5 = self.get_average_luminance_of_block(fVertical0[y * 8:y * 8 + self.N, x * 8:x * 8 + self.N])
 ##            lum6 = self.get_average_luminance_of_block(fHorizontal0[y * 8:y * 8 + self.N, x * 8:x * 8 + self.N])
 ##            lum7 = self.get_average_luminance_of_block(fVertical90[y * 8:y * 8 + self.N, x * 8:x * 8 + self.N])
@@ -123,61 +109,61 @@ class SignatureExtraction:
 ##            lum11 = self.get_average_luminance_of_block(fVertical270[y * 8:y * 8 + self.N, x * 8:x * 8 + self.N])
 ##            lum12 = self.get_average_luminance_of_block(fHorizontal270[y * 8:y * 8 + self.N, x * 8:x * 8 + self.N])
 
-            avg_lum = (lum1 + lum2 + lum3 + lum4) / 4
+        avg_lum = (lum1 + lum2 + lum3 + lum4) / 4
 
-            #std_lum = np.std(np.array([lum1, lum2, lum3, lum4, lum5, lum6, lum7, lum8, lum9, lum10, lum11, lum12]))
-            std_lum = libextraction.calculateSD(array([lum1, lum2, lum3, lum4]).ctypes.data_as(c_void_p))
+        #std_lum = np.std(np.array([lum1, lum2, lum3, lum4, lum5, lum6, lum7, lum8, lum9, lum10, lum11, lum12]))
+        std_lum = libextraction.calculateSD(array([lum1, lum2, lum3, lum4]).ctypes.data_as(c_void_p))
 
 
-            return avg_lum, std_lum
+        return avg_lum, std_lum
 
-    #@profile
-    def get_all_fragments(self):
-        fragments_list = [[],[]]
-        rot0 = self.get_blocks()
-        rot90, rot180, rot270 = self.basic_rotations(rot0)
-        counter_x = 0
-        counter_y = 0
-        append_std_lum = fragments_list[0].append
-        append_avg_lum = fragments_list[1].append
-        while(counter_x < 14 or counter_y < 14):
-            if counter_x == 15:
-                counter_y += 1
-                counter_x = counter_y
-            if counter_x == counter_y or counter_x == 14:
-                if counter_x == 14 and counter_y == 14:
-                    fragment_time = time.time()
-                    avg_lum, std_lum = self.get_fragment(rot0, rot90, rot180, rot270, counter_x, counter_y, -1)
-                    fragment_end_time = time.time()
-                    append_std_lum(std_lum)
-                    append_avg_lum(avg_lum)
-                else:
-                    fragment_time = time.time()
-                    avg_lum, std_lum = self.get_fragment(rot0, rot90, rot180, rot270, counter_x, counter_y, 1)
-                    fragment_end_time = time.time()
-                    append_std_lum(std_lum)
-                    append_avg_lum(avg_lum)
-            else:
-                avg_lum, std_lum = self.get_fragment(rot0, rot90, rot180, rot270, counter_x, counter_y, 0)
+def get_all_fragments():
+    fragments_list = [[],[]]
+    rot0 = get_blocks()
+    rot90, rot180, rot270 = basic_rotations(rot0)
+    counter_x = 0
+    counter_y = 0
+    append_std_lum = fragments_list[0].append
+    append_avg_lum = fragments_list[1].append
+    while(counter_x < 14 or counter_y < 14):
+        if counter_x == 15:
+            counter_y += 1
+            counter_x = counter_y
+        if counter_x == counter_y or counter_x == 14:
+            if counter_x == 14 and counter_y == 14:
+                fragment_time = time.time()
+                avg_lum, std_lum = get_fragment(rot0, rot90, rot180, rot270, counter_x, counter_y, -1)
+                fragment_end_time = time.time()
                 append_std_lum(std_lum)
                 append_avg_lum(avg_lum)
-            counter_x += 1
-        return fragments_list
+            else:
+                fragment_time = time.time()
+                avg_lum, std_lum = get_fragment(rot0, rot90, rot180, rot270, counter_x, counter_y, 1)
+                fragment_end_time = time.time()
+                append_std_lum(std_lum)
+                append_avg_lum(avg_lum)
+        else:
+            avg_lum, std_lum = get_fragment(rot0, rot90, rot180, rot270, counter_x, counter_y, 0)
+            append_std_lum(std_lum)
+            append_avg_lum(avg_lum)
+        counter_x += 1
+    return fragments_list
 
- 
-    def get_signature(self):
-        signature = bitarray()
-        counter_list = 0
-        counter_fragment = 0
-        sig_append = signature.append
-        for features in self.get_all_fragments():
-            for x in range(0, len(features) -1):
-                if features[x] < features[x + 1]:
-                    sig_append(True)
-                else:
-                    sig_append(False)
+def get_signature():
+    signature = bitarray()
+    counter_list = 0
+    counter_fragment = 0
+    sig_append = signature.append
+    for features in get_all_fragments():
+        for x in range(0, len(features) -1):
+            if features[x] < features[x + 1]:
+                sig_append(True)
+            else:
+                sig_append(False)
 
-        return signature
+    return signature
+
+
 
 
 
