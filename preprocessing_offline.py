8ce0f490fe9b76897b6c2e36e4dbfa5f7aa69780
Emre Yunus <Emre Yunus>
2016-12-25 21:21:56 +0100

    New changes in offline signature extraction

---
+++ preprocessing_offline.py
@@ -0,0 +1,77 @@
+from cv2 import resize, INTER_NEAREST 
+
+def set_initials_pre(L_f, image_f, counter_warped_f):
+    global L, image, counter_warped
+    L = L_f
+    image = image_f
+    counter_warped = counter_warped_f
+# class PreProcessing:
+#     def __init__(self, L, hist_eq):
+#         self.image = array([])
+#         self.L = L
+#         self.hist_eq = hist_eq
+#         self.width = 0
+#         self.height = 0
+#         self.resized_image = array([])
+#         self.warped = array([])
+
+#@profile
+def gray_image(image):
+    return cvtColor(image, COLOR_BGR2GRAY)
+
+    # def set_image(self, image):
+    #     self.image = image
+#@profile
+def get_width_height(image):
+    height, width = image.shape[:2]
+    return [width, height]
+
+#@profile
+def get_scaled(warped):
+    global resized_image
+    '''Scales image short edge to L value '''
+    warped = resize(warped, (544, 400))
+    warped = gray_image(warped)
+    warped = get_blurred(warped, 3)
+    width = 544
+    height = 400
+    new_width = 0
+    new_height = 0
+    if height > width:
+        new_height = int(floor(float(height) * (L / float(width))))
+        new_width = L
+    elif height < width:
+        new_width = int(floor(float(width) * (L / float(height))))
+        new_height = L
+    elif height == width:
+        new_width = L
+        new_height = L
+    resized_image = resize(warped, (new_width, new_height), \
+    interpolation = INTER_NEAREST)
+    #imwrite("scaled.jpg", self.resized_image)
+
+#@profile    
+def get_cropped():
+    '''Cropping image middle part L x L'''
+    get_scaled()
+    [width, height] = get_width_height(resized_image)
+    if width != height:
+        if width > height:
+            if width % 2 == 0:
+                crop_width = width / 2 - 1
+                return resized_image[:, crop_width - L / 2:crop_width + L / 2]
+            else:
+                crop_width = width / 2 - 1
+                return resized_image[:, crop_width - (L/ 2 - 1):crop_width + (L/ 2 + 1)]
+        else:
+            if height % 2 == 0:
+                crop_height = height / 2 - 1
+                return resized_image[crop_height - L / 2:crop_height + L / 2, :]
+            else:
+                crop_height = height / 2 - 1
+                return resized_image[crop_height - (L/ 2 - 1):crop_height + (L/ 2 + 1), :]
+
+
+def get_blurred(image, G):
+    '''Blurring cropped image'''
+    return GaussianBlur(image, (G, G), 0, 0)
--- signature_offline.py
+++ signature_offline.py
@@ -1,12 +1,10 @@
-from preprocessing import set_initials_pre, get_contour, get_perspective, get_cropped
+from preprocessing_offline import set_initials_pre, get_cropped
 from extraction import set_initials, get_signature
 from matching import set_initials_match, signature_rejection
 from cv2 import VideoCapture
 
 def initialize_set(image, counter):
     set_initials_pre(128, image, counter)
-    points = get_contour(3)
-    check = get_perspective(points, 0)
     set_initials(8, 4, 128, get_cropped())
     sigGen = get_signature()
     set_initials_match(sigOrig[0:238], sigGen, 24, 38, 4, 28, 22)
@@ -17,11 +15,13 @@ def initialize_set(image, counter):
 if __name__ == "__main__": 
     cap = VideoCapture("Videos/Test1.mp4")
     f = open("signature_test1.bin", "wb")
+    counter = 0
     while(cap.isOpened()):
         ret, frame = cap.read()
-        f.write(initialize_set(frame).tobytes())
         if ret != True:
             break
+
+        f.write(initialize_set(frame, counter).tobytes())
 
     cap.release()
     f.close()
