facd85c90a1d6333135b0fbe302cfdceb18142ee
Yunus Emre <yunus.kursav@gmail.com>
2017-02-10 22:02:32 +0100

    New video and new signature_offline algorithm for decreasing matching procedure errors

--- signature_offline.py
+++ signature_offline.py
@@ -1,5 +1,6 @@
 from preprocessing_offline import set_initials_pre, get_cropped
 from extraction import set_initials, get_signature
+from bitarray import bitarray
 
 from cv2 import VideoCapture
 
@@ -16,16 +17,26 @@ def initialize_set(image, counter):
 if __name__ == "__main__": 
     cap = VideoCapture("ADAPTIVE_THRESHOLD_TESTS/test17_warped.avi")
     f = open("signature_test9.bin", "wb")
-    counter = 0
+    counter = 0,
+    signature_counter = 0
+    sig_first = bitarray()
+    sig_second = bitarray()
     while(cap.isOpened()):
         ret, frame = cap.read()
         if ret != True:
             break
-	sigGen = initialize_set(frame, counter)
-        print sigGen
-        f.write(sigGen.tobytes())
-        counter += 1
-
+        if signature_counter == 0:
+            sigGen = initialize_set(frame, counter)
+            sig_first = sigGen
+            signature_counter += 1
+        else:
+            sigGen = initialize_set(frame, counter)
+            sig_second = sigGen
+            signature_res = sig_first and sig_second
+            print signature_res
+            f.write(signature_res.tobytes())
+            counter += 1
+            signature_counter = 0
     cap.release()
     f.close()
 
